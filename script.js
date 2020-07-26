addbf2555b1dad8204646afdcfe6f49848da7fb5
synthia <synthia.han@onja.org>
2020-07-26 13:03:04 +0300

    change the bacground color and  added the recipe in js

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const ingredient = document.querySelector('#ingredient');
 const steps = document.querySelector('.steps');
 const stepBtn = document.querySelector('.step-button');
 const step = document.querySelector('#step');
+const submit = document.querySelector('.submit-button');
+
 // create function for ingredient 
 const myIngredient = () => {
     const myHtmlIngredient = `
@@ -24,3 +26,45 @@ const myStep = () => {
 // add event listener
 ingredientBtn.addEventListener('click', myIngredient);
 stepBtn.addEventListener('click', myStep);
+
+
+// const recipes = [
+// 	{
+// 		title: 'Eggs',
+// 		picture: 'https://bit.ly/2ZXyiKI',
+// 		author: 'Loïc',
+// 		iculty: 'easy',
+// 		timing:diff '15',
+// 		ingredients: ['eggs', 'salt', 'water'],
+// 		steps: [
+// 			'Put a pan on the fire',
+// 			'Crack the eggs on it',
+// 			'Wait, put them out',
+// 			'Add some salt on it',
+// 		],
+// 	},
+// ];
+
+const recepiName = document.querySelector('#recepi-name');
+
+const handleSubmitButton = (event) => {
+    var recipe = ` [{
+                title: ${recepiName.value},
+                picture: '',
+                author: '',
+                iculty: '',
+                timing: "",
+                ingredients: [''],
+                steps: [""],
+            }, ];
+      
+            `;
+
+
+    function myFunction() {
+        var newArray = document.getElementsByName("name").value;
+        recipe.push(newArray);
+        document.getElementsByName("name").value = "";
+        document.getElementById("demo").innerHTML = recipe;
+    }
+}
